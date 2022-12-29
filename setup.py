06d5900716a38b0c63a50858813a74b5661c65c5
jhn-nt <angelotti.giovanni.ds@gmail.com>
2022-12-29 16:36:32 +0100

    compatibility in requires

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     name="data-snax",
     version="0.1",
     description="Versatile data ingestion pipelines for jax",
-    install_requires=["numpy>=1.23", "jax>=0.3.25"],
+    install_requires=["numpy>=1.23", "jax>=0.3.25", "requests>=2.28.1", "h5py>=3.7.0"],
     license="Apache 2.0",
     author="jhn-nt",
-    packages=["snax"]
+    packages=["snax"],
+    package_data={"snax": "datasets/*.json"},
 )
